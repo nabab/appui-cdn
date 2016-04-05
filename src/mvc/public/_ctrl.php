51006027a968461d19ecbc30be4bae2e926d9393
nabab <thomas.nabet@gmail.com>
2016-04-05 13:23:33 +0200

    db\connection to db

--- src/mvc/public/_ctrl.php
+++ src/mvc/public/_ctrl.php
@@ -2,7 +2,7 @@
 /** @var $this \bbn\mvc\controller */
 
 // SQLITE connection
-$this->data['db'] = new \bbn\db\connection([
+$this->data['db'] = new \bbn\db([
   'engine' => 'sqlite',
   'db' => '/home/mybbn/domains/cdn.mybbn.so/_appui/current/data/db/cdn.sqlite'
 ]);
