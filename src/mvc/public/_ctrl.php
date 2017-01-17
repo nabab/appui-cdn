dc90f3d708fc6eec219b75813590d0649076b194
nabab <thomas.nabet@gmail.com>
2017-01-17 07:18:42 +0100

    Added init_locale to prevent dot/comma problem

--- src/mvc/public/_ctrl.php
+++ src/mvc/public/_ctrl.php
@@ -10,7 +10,4 @@ $ctrl->data['db'] = new \bbn\db([
   'db' => BBN_CDN_DB
 ]);
 
-$ctrl->data['root'] = $ctrl->say_dir().'/';
-bindtextdomain('appui-cdn', BBN_LIB_PATH.'bbn/appui-cdn/src/locale');
-setlocale(LC_ALL, "it_IT.utf8");
-textdomain('appui-cdn');
+$ctrl->init_locale('it', 'appui-cdn');
